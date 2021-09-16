0479f7d79f93d486e1a02199f2060f5e61c18e18
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2021-09-16 10:06:37 -0300

    Cambio de etiqueta p

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Martina De León
         </p>
         <a
           className="App-link"
