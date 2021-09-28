2e7290a0cfb052047c4208231731e4446e24eb92
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2021-09-28 15:45:38 -0300

    Commiteo contador

---
+++ src/components/ItemCount.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import {Button} from 'react-bootstrap';
+
+const ItemCount = (props) => {
+    const {initial, stock} = props;
+    const [amount, setAmout] = useState(initial);
+    const [stockAvailable, setStockAvailable] = useState(stock);
+
+    const styles = {
+        width:"20%",
+        color:"white",
+        fontSize:'18px',
+        border: "1px solid white",
+        textAlign:"center",
+        margin: "3%"
+    }
+    const stylesCounter = {
+        display: "flex",
+        justifyContent:"center",
+    }
+    const stylesAmount = {width:"20%",}
+
+    const onAdd = () => {
+        if(amount < stockAvailable){
+            setAmout(amount + 1)
+        }
+    }
+    const onRemove = () => {
+        if(amount > 0){
+            setAmout(amount - 1)
+        }
+    }
+
+
+    return(
+        <div style={styles}>
+            <p>ENA Whey Protein True Made 2lb - Vainilla</p>
+            <div style={stylesCounter}>
+                <Button variant="outline-secondary" onClick={()=>onRemove()}>-</Button>
+                <p style={stylesAmount}>{amount}</p>
+                <Button variant="outline-secondary" onClick={()=>onAdd()}>+</Button>
+            </div>
+            <Button variant="outline-secondary">Agregar al carrito</Button>
+        </div>
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,13 +1,10 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
 const ItemListContainer = () => {
-    const styles = {
-        marginTop: '80px'
-    }
     return( 
-    <div style={styles}>
-        <h1>Próximamente...</h1>
-        <h2>¡Gracias por esperarnos!</h2>
+    <div>
+        <ItemCount initial={1} stock={20}/>
     </div>
     )
 }
